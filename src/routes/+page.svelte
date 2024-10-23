efe04e03ae664700eaf23892bb833acbff6a8bba
Benjamin S. Leveritt <benajmin@leveritt.co.uk>
2024-10-23 17:32:31 +0100

    Add type scale

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,15 @@
 </script>
 
 <h1>Benjamin S. Leveritt</h1>
-<h2>Design Engineer. Optimistic futurist. Budding essayist.</h2>
+<h4>Design, Engineering, Learning. Optimistic futurist. Budding essayist.</h4>
 
 <Hr />
+
+<p>
+	Hello! I'm Ben. I'm a designer, engineer, and learner. I'm also an optimistic futurist and budding
+	essayist.
+</p>
+
+<p>
+	I have a <a href="/now">now</a> page, if you're into that sort of thing.
+</p>
