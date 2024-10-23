af47f594ffe6809278214a273a71f107ddc57b20
Benjamin S. Leveritt <benajmin@leveritt.co.uk>
2024-10-23 14:27:16 +0100

    Add basic style

---
+++ src/lib/components/Hr.svelte
@@ -0,0 +1,9 @@
+<hr />
+
+<style>
+	hr {
+		border: none;
+		border-top: 4px solid white;
+		margin: var(--spacing-large) 0;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script lang="ts">
+	import Hr from '$lib/components/Hr.svelte';
+</script>
+
 <h1>Benjamin S. Leveritt</h1>
-<h2>Design Engineer. Budding essayist.</h2>
+<h2>Design Engineer. Optimistic futurist. Budding essayist.</h2>
+
+<Hr />
