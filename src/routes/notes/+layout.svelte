45551089482431b11448257c598295657962a8c3
Benjamin S. Leveritt <benjamin@leveritt.co.uk>
2025-07-28 11:58:38 +0100

    Adds Notes and Portfolio routes

---
+++ src/routes/notes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	const { children } = $props();
+	import Page from '$lib/components/Page.svelte';
+</script>
+
+<Page>
+	<header>
+		<nav>
+			<a href="/">Home</a>
+			<a href="/portfolio">Portfolio</a>
+			<a href="/notes">Notes</a>
+		</nav>
+	</header>
+	{@render children()}
+</Page>
