c420ea82c42e1191b73fc398d0a954bbd5edb535
Benjamin S. Leveritt <benjamin@leveritt.co.uk>
2025-07-30 09:57:15 +0100

    Updates asciidoc preprocessor

--- src/lib/preprocessors/asciidoc.js
+++ src/lib/preprocessors/asciidoc.js
@@ -5,18 +5,30 @@ import asciidoctor from 'asciidoctor'
  * @param {string} content - The AsciiDoc content to convert
  * @returns {{code: string}} - Object containing the HTML output
  */
-function html(content) {
+export function html(content) {
   const Asciidoctor = asciidoctor()
   const html = Asciidoctor.convert(content, { attributes: { 'showTitle': true}})
   return { code: html.toString() }
 }
 
+/**
+ * Converts AsciiDoc content to HTML
+ * @param {string} filename - The filename of the AsciiDoc
+ */
+export function loadFile(filename) {
+  const Asciidoctor = asciidoctor()
+  return Asciidoctor.loadFile(filename, { attributes: { 'showTitle': true}})
+}
+
+/**
+ * Returns a SvelteKit preprocessor for AsciiDoc
+ * @returns {{ name: string, markup: (params: { content: string, filename: string }) => { code: string } | undefined }}
+ */
 function asciidoc() {
   return {
     name: 'asciidoc',
     markup({ content, filename }) {
       if (filename.endsWith('.asciidoc')) {
-        console.log(filename);
         return html(content);
       }
     }
